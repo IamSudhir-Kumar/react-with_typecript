a3fbb3c64143c0a002dbc9eb221d10790d1a5524
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-04-02 13:08:44 +0530

    new hindi vedio practice

---
+++ practice3/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  constructor()
+  {
+    
+  }
+  return (
+    <div className="App">
+      
+      <h1>Hide / show</h1>
+      <button onClick={()=>{alert("test")}}>Clickme </button>
+    </div>
+  );
+}
+
+export default App;
