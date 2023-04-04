c916b924ab4c63e83d0c79efbe99306d2c750ea1
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-04-04 21:52:14 +0530

    new file

--- practice3/src/App.js
+++ practice3/src/App.js
@@ -2,16 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  constructor()
-  {
-    
-  }
-  return (
-    <div className="App">
-      
-      <h1>Hide / show</h1>
-      <button onClick={()=>{alert("test")}}>Clickme </button>
-    </div>
+  
   );
 }
 
