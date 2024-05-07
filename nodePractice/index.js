0b0a2e0d6d7c97cdcacc9f5281ccdccaba3d2394
SUDHIR KUMAR <167057380+Sudhir-Kumar-Gamersback@users.noreply.github.com>
2024-05-07 11:28:39 +0530

    chore: Remove unnecessary files and code

--- nodePractice/index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const {pie} = require('./new.js');
-const nodePractice = process.argv[2];
-const newPractice = {
-    content : nodePractice,
-    id : pie
-}
-console.log(newPractice);
